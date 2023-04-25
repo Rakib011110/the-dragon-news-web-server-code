5fa07c61d222aced6bf23d05655b68f84dfda76c
Rakibul Islam <rakib011110@gmail.com>
2023-04-25 22:09:03 +0600

    news id add

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require("cors")
 app.use(cors())
 
 const categories = require("./data/categories.json")
+const news = require("./data/news.json")
 // console.log(categories);
 
 app.get("/", (req, res) => {
@@ -16,6 +17,28 @@ app.get("/", (req, res) => {
 app.get('/categories', (req, res) => {
     res.send(categories)
 
+})
+app.get("/news", (req, res) => {
+    res.send(news)
+})
+
+app.get("/news/:id", (req, res) => {
+    const id = req.params.id
+    console.log(id);
+    const selectedId = news.find(n => n._id === id)
+    res.send(selectedId)
+})
+
+app.get("/categories/:id", (req, res) => {
+    const id = parseInt(req.params.id)
+    // console.log(id)
+    if (id === 0) {
+        res.send(news)
+    }
+    else {
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id)
+        res.send(categoryNews)
+    }
 })
 
 app.listen(port, () => {
